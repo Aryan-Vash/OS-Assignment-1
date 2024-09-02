ff6ad8fb787068c0c27285b1a6e3f02f8c174ddc
samieeew <159107362+samieeew@users.noreply.github.com>
2024-09-02 21:43:14 +0530

    Update loader.c

--- loader.c
+++ loader.c
@@ -13,9 +13,9 @@ void loader_cleanup() {
 }
 
 void load_and_run_elf(char** exe) {
-
-  fd = open(exe[1], O_RDONLY);
-
+    fd = open(exe[1], O_RDONLY);
+  // 1. Load entire binary content into the memory from the ELF file.
+  
   if(fd<0){
     perror("open");
     exit(1);
@@ -24,6 +24,8 @@ void load_and_run_elf(char** exe) {
   ehdr = (Elf32_Ehdr*)malloc(sizeof(Elf32_Ehdr));
   read(fd, ehdr, sizeof(Elf32_Ehdr));
 
+  // 2. Iterate through the PHDR table and find the section of PT_LOAD 
+  //    type that contains the address of the entrypoint method in fib.c
   lseek(fd, ehdr->e_phoff, SEEK_SET);
   phdr = (Elf32_Phdr*)malloc(ehdr->e_phentsize*ehdr->e_phnum);
   read(fd,phdr,sizeof(Elf32_Phdr));
@@ -44,6 +46,8 @@ void load_and_run_elf(char** exe) {
     exit(1);
   }
 
+  // 3. Allocate memory of the size "p_memsz" using mmap function 
+  //    and then copy the segment content
   void *mem = mmap(NULL, phdr_load->p_memsz, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, fd, phdr_load->p_offset);
   if (mem == MAP_FAILED) {
       perror("mmap erroe");
@@ -54,10 +58,13 @@ void load_and_run_elf(char** exe) {
   lseek(fd, phdr_load->p_offset, SEEK_SET);
   read(fd, mem, phdr_load->p_memsz);
 
+  // 4. Navigate to the entrypoint address into the segment loaded in the memory in above step
+  // 5. Typecast the address to that of function pointer matching "_start" method in fib.c.
   int (*_start)()= (int(*)()) mem + (ehdr->e_entry - phdr_load->p_vaddr);
 
   // int (*_entry)()= (int(*)())_start;
 
+  // 6. Call the "_start" method and print the value returned from the "_start"
   int result = _start();
 
   printf("User _start return value = %d\n",result);
